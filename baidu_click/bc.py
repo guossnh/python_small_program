3db009cdba5feee8e4f27fcf402783f3f2bd47bf
tlk <guossnh@gmail.com>
2016-06-21 10:36:50 +0800

    更新了几个方法,就酱

--- baidu_click/bc.py
+++ baidu_click/bc.py
@@ -13,7 +13,7 @@ import random #导入随机数包
 import string #导入string包
 import urllib
 import binascii
-
+import csv  #引入csv文件
 
 #这里放一些变量
 kewword = ""
@@ -82,17 +82,36 @@ def tryproxy():
 
 
 
+#这个方法主要是页面移动停留的办法   想想我觉得这个方法是骗自己的 https的代理  不是 http的代理
+def pageReader():
+    global driver#全局变量
+    time.sleep(3) #先休息3秒防止出错
+    num = 180   #固定值是180根据百度搜多搜索页面高度1879
+    for x in range(1,int(random.uniform(4,10))): #循环  设置循环表示要跳转几次. 上线 的话 可以多添加 几次
+        time.sleep(2)   #每次循环之后 添加停留时间  增加容错率
+        if random.sample([0,1],1) == 1:     #这条判断是随机表示跳转
+            num = num + 180  
+        else:
+            num = abs(num - 180)
+        #num = abs(num + int(random.sample([180,-180],1)))   #这种写法更吊  但是  出错  有时间看看 修改一下
+        driver.execute_script("window.scrollBy(0,%s)" % num,"") #最后了  开始 跳转  就酱
+
+
+#百度搜索开始搜索方法
+def baidustart(kew):
+    global driver , kewword , baiduTitle #引入全局变量
+    kewword = kew
+    time.sleep(5)
+    driver.get("https://www.baidu.com/s?wd="+kewword)#开始搜索关键字
+    driver.maximize_window()#浏览器最大化(这个 可选)
+    assert baiduTitle in driver.title#确定页面是百度搜索页面
 
 
 
 
-
-
-
-
-
-
-
+#这个办法主要是 放一些 排除 的网站,然后  其他 的网站   都要 点击
+def noClick():
+    driver.execute_script("$('a:contains(315jiage.cn)').parent().parent().remove()","") #排除315价格网
 
 
 
@@ -114,9 +133,58 @@ def suiji(sum = 7):
     else:
         return False
 
+
+#单页面的浏览功能
+def baidulist():
+    time.sleep(5) #时间停留5秒 增加系统容错率
+    content_list_num = driver.find_elements_by_tag_name("h3")
+    print("打印出  总共  有几个元素:%s"%len(content_list_num))
+    for x in content_list_num:
+        try:
+            x.find_element_by_tag_name("a").click()
+            time.sleep(5)
+            driver.switch_to_window(driver.window_handles[-1])
+            seePageHead()
+            driver.close()
+            driver.switch_to_window(driver.window_handles[0])
+            time.sleep(5)
+        except:
+            print("wrong")
+    print("it is over")
+
+
+#下边是两套规则
+#规则1  除了过滤器其他的 都点
+def type1():
+    print("ok1")
+#规则2  点击特定的页面   其他的不点
+def type2():
+    print("ok2")
+
+
+def readFile():
+    with open('date.csv', "rt" , encoding="utf8") as f:
+        f_csv = csv.reader(f)
+        headers = next(f_csv)
+        for row in f_csv:
+            if row[0]=='1':
+                type1()
+            else:
+                type2()
+
+
+
+
+
 #主程序
 def main():
-    print(suiji())
+    
+    #tryproxy()  #查找代理 并且设置
+
+    #读取配置文件获取
+    readFile()
+
+
 
 if __name__ == '__main__':
     main()
