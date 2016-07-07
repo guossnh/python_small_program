8cb52dbb3c827ff8a1f9d06699f7259c828eb3af
tlk <guossnh@gmail.com>
2016-07-07 15:01:30 +0800

    over  hhha

--- gaode_get/man.py
+++ gaode_get/man.py
@@ -11,13 +11,14 @@ from urllib import request
 import sqlite3
 import json
 import csv
+from collections import Iterable
 
 #常量 
 #洗浴推拿 编号 071400  
 #诊所  编号 090300
 
-#  c07150c4cb806794cd91568a83056720,5b948aa8ff7778b745c04639d6d4517f
-gaode_key = "5b948aa8ff7778b745c04639d6d4517f"
+#319b295774592eb15f74feb5a116f5ba,a173ef77f76f2573a2f1e1a159af29b5,49986bc0637849b6d083d0329ba98ab8,b596394af04fa0bb6c76b47fbed45295
+gaode_key = "49986bc0637849b6d083d0329ba98ab8"
 keywords = ""
 gaode_link = ""
 goade_link_other_kew = "&offset=50&citylimit=true&types=071400"
@@ -28,8 +29,11 @@ conn = sqlite3.connect('gaode.db')
 
 now_link = ""
 
-pagenumber = 1
+pagenumber = 1 #这是页面数的标记
 
+city_num = 1 #这是变量选择城市的标记
+
+this_is_all_num = 0#用于判断次数,判断一个key的话  每天使用次数的话 必须小于1000次
 
 
 """
@@ -41,53 +45,64 @@ pagenumber = 1
 
 
 def getlink():
-    global now_link
+    global now_link , pagenumber , city , goade_link_other_kew , gaode_key
     now_link = "http://restapi.amap.com/v3/place/text?%s&key=%s&city=%s&page=%s"%(goade_link_other_kew,gaode_key,city,pagenumber)
+    pagenumber = pagenumber + 1
     return now_link
 
 
 #把城市的编号存入数据库,调用方法的时候输入rowid 返回对应的城市编号 并且设置当前全局变量 'city' 的值
 def get_city_number(number):
-    global city
-    data = conn.execute("select num from city where rowid = %s" % number)
-    for row in data:
-        city = row[0]
-        return row[0]
-
-def tryit():
-    with urllib.request.urlopen('  ') as f:
-         print(f.read())
+    global city , city_num
+    c = conn.cursor()
+    c.execute("select num from city where rowid = %s" % number)
+    data=c.fetchone()
+    if data is None:
+        print("程序结束了")
+        conn.close()
+        return None
+    else:
+        print(data[0])
+        city = data[0]
+        return data[0]
 
 
 #通过通过json数据的解析  把每一条数据存入数据库
 def do_json():
-    with open('date.json', 'r' , encoding="utf8") as f:
-        data = json.load(f)
-        print(len(data['pois']))
-        # for row in data['pois']:
-        #     dbadd(row["id"],row["name"],row["type"],row["typecode"],row["biz_type"],row["address"],row["tel"],row["distance"],row["biz_ext"],row["pname"],row["cityname"],row["adname"])
-        
+    global now_link , pagenumber , city_num
+    with urllib.request.urlopen(now_link) as f:
+        get_json_data = f.read().decode('UTF-8')
+        json_data = json.loads(get_json_data)
+        json_data_len = len(json_data['pois'])#获取数据长度来控制链接
+        if json_data_len > 0:
+            for row in json_data['pois']:
+                dbadd(row["id"],row["name"],row["type"],row["typecode"],row["biz_type"],row["address"],row["tel"],row["distance"],row["biz_ext"],row["pname"],row["cityname"],row["adname"])
+        else:
+            print("结束一个城市")
+            pagenumber = 1#重置页面号码
+            city_num  = city_num + 1
+            get_city_number(city_num)
 
 
 def dbadd(gdid,name,gdtype,typecode,biz_type,address,tel,distance,biz_ext,pname,cityname,adname):
     try:
         conn.execute("insert into gaode (gdid,name,gdtype,typecode,biz_type,address,tel,distance,biz_ext,pname,cityname,adname) values ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" %(gdid,name,gdtype,typecode,biz_type,address,tel,distance,biz_ext,pname,cityname,adname))
         conn.commit()
-        print("add one")
     except:
-        print("not add")
-    
-
-
+        pass
 
 #主函数
 def main():
-
-    #conn.close()
-    #do_json();
-    getlink()
-    print(get_city_number(2))
+    global this_is_all_num , pagenumber , city_num
+    get_city_number(city_num)
+    while (this_is_all_num < 990):
+        getlink()
+        this_is_all_num = this_is_all_num + 1 #控制计数保证总数量小于1000
+        do_json()#执行方法
+    print("结束标记  城市rownum = '%s'  页面编号 = '%s'" %(city_num , pagenumber))
     conn.close()
+
+
 
 
 if __name__ == '__main__':
@@ -103,4 +118,10 @@ if __name__ == '__main__':
         for row in f_csv:
             conn.execute("insert into city (num) values (%s)" % row[0])
         conn.commit()
+
+    with open('date.json', 'r' , encoding="utf8") as f:
+     data = json.load(f)
+     print(len(data['pois']))
+     for row in data['pois']:
+         dbadd(row["id"],row["name"],row["type"],row["typecode"],row["biz_type"],row["address"],row["tel"],row["distance"],row["biz_ext"],row["pname"],row["cityname"],row["adname"])
 """
