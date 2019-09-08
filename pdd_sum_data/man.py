0306448170e778b4a0d2cb2428009596358d53d7
guossnh <guossnh@gmail.com>
2019-09-08 08:18:56 +0800

    add

---
+++ pdd_sum_data/man.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#-*- coding : utf-8 -*-
+#这个主要是谢同学使用的每天根据每个店铺导出的数据文档然后分析昨天的销量然后统计给boss
+import os
+
+
+class do_data_by_remark:
+    def __init__(self):
+        pass
+    #完成如下
+    #远程获取验证信息。通过之后开始计算
+    #
+    #下边的是全局变量
+    ganyu_list = []#这个是刷单的数据
+    file_link = "C:\\csvdata\\"
+
+    def get_file_list(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+    if __name__ == "__main__":
+        pass
