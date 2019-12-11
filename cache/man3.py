94be55ea03725d773e8a1c560797b3c7e2433872
guossnh <guossnh@gmail.com>
2019-12-11 23:14:34 +0800

    add

---
+++ cache/man3.py
@@ -0,0 +1,63 @@
+#-*- coding : utf-8 -*-
+
+import xlwt,xlrd,csv,os
+import  pandas as pd
+
+
+desktop_link = "C:\\Users\\Administrator\\Desktop\\"
+path =  "C:\\Users\\Administrator\\OneDrive\\save\\save\\"
+
+#读取的时候判断    乳  子宫 暖宫   皮肤  止痒 痛经  女
+
+def readexcle():
+    book = xlrd.open_workbook("C:\\Users\\Administrator\\OneDrive\\save\\save\\maideduo\\2019-11-24-2019-12-11已发货查询结果3071.xlsx")
+    booksheet = book.sheet_by_index(0)
+    rows=booksheet.get_rows()
+    wocao = 0
+    for row in rows:
+        if("乳" in row[7].value or "宫" in row[7].value or "女" in row[7].value or "脱皮" in row[7].value or "痛经" in row[7].value):
+            try:
+                writecsv(row[4].value,row[5].value,row[6].value)
+                wocao+=1
+            except:
+                pass
+            else:
+                pass
+        
+#写入csv
+
+def do_csv():
+    with open("C:\\Users\\Administrator\\OneDrive\\save\\save\\luoyangwangcaoshengwujishu\\ExportOrderList201912111052.csv","r") as file:
+        re = csv.reader(file)
+        content =0
+        content2 =0
+        for x in re:
+            content2 +=1
+            if ("乳" in x[19] or "宫" in x[19] or "女" in x[19] or "脱皮" in x[19] or "痛经" in x[19]or "洗" in x[19]):
+                content+=1
+        print(content)
+        print(content2)
+
+
+
+
+def findfile():
+    file_all = []
+    file_csv = []
+    for root, dirs, files in os.walk(path):
+        file_all = file_all+ files
+    for x in file_all:
+        if("OrderList" in  x):
+            file_csv.append(x)
+    return file_csv
+
+
+
+
+def writecsv(name,phone,address):
+    with open(""+desktop_link+"result.csv","a+",newline = '') as csvfile: 
+        writer = csv.reader(csvfile)
+        writer.writerow([name,phone,address])
+
+
+do_csv()
