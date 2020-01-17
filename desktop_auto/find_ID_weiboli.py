4d2013e5a35f3005c34649d95cfdcc9f0aecbcb9
guossnh <guossnh@gmail.com>
2020-01-17 10:03:19 +0800

    add

--- desktop_auto/find_ID_weiboli.py
+++ desktop_auto/find_ID_weiboli.py
@@ -12,19 +12,9 @@ user_data = r"--user-data-dir=C:\\Users\\Administrator\\AppData\\Local\\Google\\
 
 #打开chrome浏览器并且载入现在正在使用的配置
 
-#option = webdriver.ChromeOptions()
+option = webdriver.ChromeOptions()
 #option.add_argument("--user-data-dir="+r"C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1")
-#driver = webdriver.Chrome(chrome_options=option)
-#driver.get(weiboli_url)
-#assert "微薄利" in driver.title
-#time.sleep(5)
-
-
-from selenium.webdriver import ChromeOptions
-
-options = ChromeOptions()
-options.debugger_address = "127.0.0.1:" + '8888'
-browser = webdriver.Chrome(chrome_options=options,chrome_driver)
-browser.get("https://mms.pinduoduo.com/home/")
-assert "拼多多" in browser.title
+driver = webdriver.Chrome(chrome_options=option)
+driver.get(weiboli_url)
+assert "微薄利" in driver.title
 time.sleep(5)
