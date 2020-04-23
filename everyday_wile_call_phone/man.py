b2d7dfad68673b477df3ebc031cb4ae319888569
guossnh <guossnh@gmail.com>
2020-04-23 11:16:31 +0800

    add

--- everyday_wile_call_phone/man.py
+++ everyday_wile_call_phone/man.py
@@ -44,22 +44,38 @@ def write_result():
     oneday_data = pd.merge(im_pd, getID_pd, how='left', left_on='商品id',right_on='产品ID')
     oneday_data = oneday_data[["订单号","商品id","手机","店铺","姓名","产品简称"]]
     oneday_data = oneday_data[oneday_data["姓名"].notnull()]#去除不需要统计的值
-    oneday_data.to_csv(""+man_url+"这里看结果文件\\cha1.csv",sep=',',index=False)
+    #oneday_data.to_csv(""+man_url+"这里看结果文件\\cha1.csv",sep=',',index=False)
+
     #获取之前记录的对象的值
     allday_data = pd.read_csv(""+man_url+"all_data.csv")
-    allday_data = allday_data[["订单号","商品id","手机","店铺","姓名","产品简称"]]
     oneday_data = oneday_data.append(allday_data)
     oneday_data = oneday_data.append(allday_data)
     oneday_data = oneday_data.drop_duplicates(subset=['订单号'],keep=False)
-    oneday_data.to_csv(""+man_url+"这里看结果文件\\cha.csv",sep=',',index=False)
-    
-    new_all_file = pd.merge(allday_data, oneday_data ,how="outer", on="订单号")
-    new_all_file = new_all_file.drop_duplicates(subset=['订单号'],keep="first")
-    print(new_all_file)
-    new_all_file.to_csv(""+man_url+"all_data.csv",sep=',',index=False)
+    now_time = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    oneday_data.to_csv(""+man_url+"这里看结果文件\\"+now_time+"需要处理的电话号码.csv",sep=',',index=False)
+    #追加差值文件到总数据文件
+    allday_data = allday_data.append(oneday_data)
+    #allday_data.to_csv(""+man_url+"all_data.csv",sep=',',index=False)
+
+    #生成需要发短信的文件
+    for row in getID_pd.itertuples():
+        pname = getattr(row, '姓名')
+        shop = getattr(row, '店铺')
+        ename = getattr(row, '产品简称')
+        one_product = oneday_data[(oneday_data["姓名"]==pname)&(oneday_data["店铺"]==shop)&(oneday_data["产品简称"]==ename)]
+        if(one_product.empty):
+            pass
+        else:
+            with open(""+man_url+"这里看结果文件\\"+now_time+"需要发短信的数据.txt","a+") as f:
+                f.write(""+pname+shop+"的"+ename+"\n")
+                phone_list = ""
+                for row in one_product.itertuples():
+                    phone_list = phone_list +","+ getattr(row, '手机').strip()
+                f.write(phone_list[1:]+"\n")
+                f.close()
 
 
-    #oneday_data.to_csv(""+man_url+"all_data.csv",sep=',')
     
 
 if __name__ == "__main__":
