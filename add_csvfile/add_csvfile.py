ad6f7f844422cc7d663e138e5d96b28670b546fd
guossnh <guossnh@gmail.com>
2021-01-30 10:03:41 +0800

    add

--- add_csvfile/add_csvfile.py
+++ add_csvfile/add_csvfile.py
@@ -2,12 +2,19 @@
 #这个是一个通过合并csv的程序，帮助会计合并csv文件
 
 
-import pandas,flask,os,time
+import pandas,os,time
+from flask import Flask
+from flask import render_template
 
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home(name=None):
+    return render_template('index.html', name=name)
 
 def man():
     pass
 
-
 if __name__ == "__main__":
-    man()
+    app.run()
