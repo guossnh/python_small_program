1bb58d793754fab60708e06c45be3db49569da16
guossnh <guossnh@gmail.com>
2021-10-10 18:13:18 +0800

    add

--- make_DY_data/make_dy_data.py
+++ make_DY_data/make_dy_data.py
@@ -4,7 +4,7 @@
 #计算抖音店铺销量的程序
 
 import pandas as pd
-import glob,os,openpyxl,re
+import glob,os,openpyxl,re,platform
 import urllib.request  
 
 man_URL = "d:\\应用\\make_TB_data\\"
@@ -22,6 +22,28 @@ def kaiguan():
     else:
         return False
 
+#获取执行程序的文件夹的路径
+def get_file_folder():
+    #fielpath = os.getcwd()
+    global man_URL
+    folder_list = os.listdir(os.path.dirname(__file__))
+    if("file" in folder_list):
+        print("修改工作路径")
+        man_URL = os.path.dirname(__file__).replace("\\","\\\\")+"\\\\"
+    else:
+        print("调试路径")
+
+#判断系统版本然后修改对应的工作路径
+def find_version_of_OS():
+    sysstr = platform.system()
+    if(sysstr =="Windows"):
+        get_file_folder()
+    elif(sysstr == "Linux"):
+        print ("Call Linux tasks")
+    else:
+        print ("Other System tasks")
+
+
 
 def make_data():
     pass
@@ -31,8 +53,13 @@ def make_data():
 if __name__ == "__main__":
     if(kaiguan()):
         print("访问正常")
+        #开始判断修改操作区路径
+        find_version_of_OS()
+          
+
+
+
         make_data()
-        print(os.getcwd())
-        #read_product_excle()
+
     else:
         print("无法获取配置文件")
